19db10885c3c5e03824deac281c3f7500de37628
natecarson94 <33134997+natecarson94@users.noreply.github.com>
2018-01-15 08:24:16 -0500

    fixed heroku deployement

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Nate>:rain543>@ds157057.mlab.com:57057/node-todo-api'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
